bf94663add0330763d81e715e457b7df1b6243de
takumitak <161606155+takumitak@users.noreply.github.com>
2024-03-13 14:11:22 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.lang.Math;;
+
+public class Main {
+  public static void main(String[] args) {
+    Random li = new Random();
+    int suma = 0;
+    int liran = 0;
+    for(int i = 0; i < 5; i++){
+      liran = li.nextInt(100);
+      System.out.println(liran);
+      suma = suma + (int)Math.pow(liran, 2);
+    }
+    System.out.println(suma);
+
+    
+    //System.out.println("Hello world!");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
